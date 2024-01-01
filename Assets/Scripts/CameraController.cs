7f19c38d363c8768b1b9880ee9d8cd9956b3fdfa
nickyxxxl <nickyswinckels@yahoo.com>
2024-01-01 17:19:43 +0100

    Implemented Smooth Camera
    The interpolation is linear, it can be improved by using a
    higher order formula.
    
    But this looks pretty good.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private float cameraDistance = -10f;
+    [SerializeField] private float minSpeed = 10f;
+    [SerializeField] private float minDistance = 0.5f;
+    [SerializeField] private float fractionSpeed = 0.2f;
+    private Vector2 diff = new Vector2();
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, cameraDistance);
+        // Camera follows player directly
+        // transform.position = new Vector3(player.position.x, player.position.y, -10f);
+
+        // Smooth follow
+        diff = player.position - transform.position;
+
+        if(diff.magnitude > minDistance) {
+            float speed = Mathf.Max(fractionSpeed*diff.magnitude, minSpeed);
+            Vector2 delta = diff*((speed*Time.deltaTime)/diff.magnitude);
+            transform.position = transform.position + new Vector3(delta.x, delta.y, 0);
+        }
     }
 }
